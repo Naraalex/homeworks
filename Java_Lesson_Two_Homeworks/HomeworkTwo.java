ec53cd828da40406d2d29f7fd61e4ef26a729b02
Narine Aleksanyan <narine.aleksanyan88@gmail.com>
2023-03-27 22:54:25 +0400

    add Homework 2

---
+++ Java_Lesson_Two_Homeworks/HomeworkTwo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class HomeworkTwo {
+	public static void main (String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("mutq ara 1-ic minchev 10-y cankacac tiv - ");
+		
+		int number = sc.nextInt();
+		
+		if(number > 0 && number < 10) {
+			System.out.println(number*number);
+		}else {
+			System.out.println("Sxal haxordagrutyun");
+		}
+	}
+}
